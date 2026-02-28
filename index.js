30a29b0ecf4e7408b787dc1daf2e0d34edf358b8
beshoy <beshoyagaiby676@gmail.com>
2026-03-01 01:00:11 +0200

    fix production port and start script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv"
 
 // import morgan from 'morgan';
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 dotenv.config()
 app.use(express.json());
 
